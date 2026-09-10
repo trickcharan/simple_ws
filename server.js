4494dd23fe567d51cdd3f48217080dd47eab432c
Vishlaks <vishlaks@cisco.com>
2026-09-10 21:18:05 +0530

    Add WebSocket support

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("node:http");
+const { WebSocketServer } = require("ws");
 
 function loadVirtualAgents() {
   const rawAgents = process.env.VIRTUAL_AGENTS_JSON || "[]";
@@ -35,6 +36,29 @@ const server = http.createServer((request, response) => {
     response.writeHead(500, { "Content-Type": "application/json" });
     response.end(JSON.stringify({ error: error.message }));
   }
+});
+
+const webSocketServer = new WebSocketServer({ noServer: true });
+
+webSocketServer.on("connection", (socket) => {
+  socket.send("WebSocket connected");
+
+  socket.on("message", (message) => {
+    socket.send(`Echo: ${message}`);
+  });
+});
+
+server.on("upgrade", (request, socket, head) => {
+  const { pathname } = new URL(request.url, "http://localhost");
+
+  if (pathname !== "/ws") {
+    socket.destroy();
+    return;
+  }
+
+  webSocketServer.handleUpgrade(request, socket, head, (webSocket) => {
+    webSocketServer.emit("connection", webSocket, request);
+  });
 });
 
 const port = Number(process.env.PORT || 10000);
